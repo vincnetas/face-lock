de14fad37429e49d715096c48198f046507a223e
Vincentas <vincentas.vienozinskis@deeper.eu>
2018-08-14 17:56:22 +0300

    opencv

--- src/main/java/lt/vin/face/FaceApplication.java
+++ src/main/java/lt/vin/face/FaceApplication.java
@@ -1,15 +1,21 @@
 package lt.vin.face;
 
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter.ToIplImage;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.github.sarxos.webcam.Webcam;
+
 
 @SpringBootApplication
 public class FaceApplication implements CommandLineRunner {
@@ -20,11 +26,18 @@ public class FaceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Webcam.setDriver(RpiDriver.class);
+		//Webcam.setDriver(RpiDriver.class);
 		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(new Dimension(3280, 2464));
+		
+		//webcam.setViewSize(new Dimension(3280, 2464));
 		webcam.open();
-		ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
+		BufferedImage bim = webcam.getImage();
+		ImageIO.write(bim, "PNG", new File("hello-world.png"));
+		
+		ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+	    Java2DFrameConverter java2dConverter = new Java2DFrameConverter();
+	    IplImage iplImage = converter.convert(java2dConverter.convert(bim));
+
 		
 	}
 }
